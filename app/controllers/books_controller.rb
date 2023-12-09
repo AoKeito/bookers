9489862fc999a3b3a71e7084466c962335c94d3c
EC2 Default User <149554348+AoKeito@users.noreply.github.com>
2023-12-09 10:40:20 +0000

    編集機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def index
     @books = Book.all
@@ -18,7 +15,20 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end 
   
   def book_params
     params.require(:book).permit(:title, :body)
